ed3037a228afc0f7e720be158158c888b1e80f86
Michael Ducharm <ducharmdev@gmail.com>
2021-07-23 17:21:31 -0400

    added basic express server

---
+++ graphql/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+
+const app = express();
+
+app.use(postgraphile(
+    process.env.DATABASE_URL || "postgres://user:pass@host:5432/dbname",
+    "public",
+    {
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true,
+    }
+));
+
+app.listen(process.env.PORT || 3000);
+
