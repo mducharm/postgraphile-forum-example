4e231fa18c1cff19a8e975dce1e2414afc345e24
Michael Ducharm <ducharmdev@gmail.com>
2021-07-24 16:27:18 -0400

    latest post func

--- db/init/02-post.sql
+++ db/init/02-post.sql
@@ -42,3 +42,15 @@ returns text as $$
 $$ language sql stable;
 
 comment on function app_public.post_summary(forum_example.post, int, text) is 'A truncated version of the body for summaries.';
+
+create function 
+    app_public.person_latest_post(person app_public.person)
+returns app_public.post as $$
+    select post.* 
+    from app_public.post as post
+    where post.author_id = person.id
+    order by created_at desc
+    limit 1
+$$ language sql stable;
+
+comment on function app_public.person_latest_post(app_public.person) is 'Get’s the latest post written by the person.';
