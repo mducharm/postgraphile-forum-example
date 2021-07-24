bca7844af669c8ef5e87a786fa0cb66bbfcce2f8
Michael Ducharm <ducharmdev@gmail.com>
2021-07-24 16:40:45 -0400

    added updated_at fields

--- db/init/01-person.sql
+++ db/init/01-person.sql
@@ -7,7 +7,8 @@ create table app_public.person (
     first_name text not null check (char_length(first_name) < 80),
     last_name text check (char_length(first_name) < 80),
     about text,
-    created_at timestamp default current_timestamp
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default now() -- same as current_timestamp
 );
 
 comment on table app_public.person is 'A user of the forum.';
@@ -16,6 +17,7 @@ comment on column app_public.person.first_name is 'The person’s first name.';
 comment on column app_public.person.last_name is 'The person’s last name.';
 comment on column app_public.person.about is 'A short description about the user, written by the user.';
 comment on column app_public.person.created_at is 'The time this person was created.';
+comment on column app_public.person.updated_at is 'The time this person was updated.';
 
 /* Functions */
 
@@ -27,3 +29,6 @@ $$ language sql stable;
 -- note: stable keyword signifies that this function does not mutate the database.
 
 comment on function app_public.person_full_name(app_public.person) is 'A person’s full name which is a concatenation of their first and last name.';
+
+/* Triggers */
+
--- db/init/02-post.sql
+++ db/init/02-post.sql
@@ -15,8 +15,10 @@ create table app_public.post (
     headline text not null check (char_length(headline) < 200),
     body text,
     topic app_public.post_topic,
-    created_at timestamp default now()
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
+
 
 comment on table app_public.post is 'A forum post written by a user.';
 comment on column app_public.post.id is 'The primary key for the post.';
@@ -25,6 +27,7 @@ comment on column app_public.post.author_id is 'The id of the author user.';
 comment on column app_public.post.topic is 'The topic this has been posted in.';
 comment on column app_public.post.body is 'The main body text of our post.';
 comment on column app_public.post.created_at is 'The time this post was created.';
+comment on column app_public.post.updated_at is 'The time this post was updated.';
 
 /* Functions */
 
