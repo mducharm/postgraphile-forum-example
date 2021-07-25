04e71147870231ffcbdfab769bf6008acc550eb2
Michael Ducharm <ducharmdev@gmail.com>
2021-07-24 21:43:48 -0400

    updated env vars

--- graphql/src/server.js
+++ graphql/src/server.js
@@ -5,12 +5,13 @@ const app = express();
 
 app.use(postgraphile(
     process.env.DATABASE_URL || "postgres://user:pass@host:5432/dbname",
-    "public",
+    process.env.PUBLIC_SCHEMA || "public",
     {
         watchPg: true,
         graphiql: true,
         enhanceGraphiql: true,
-        retryOnInitFail: true
+        retryOnInitFail: true,
+        pgDefaultRole: process.env.DEFAULT_ROLE || "forum_anon"
     }
 ));
 
